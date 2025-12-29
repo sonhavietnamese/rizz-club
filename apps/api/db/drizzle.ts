eb6730d992ef456ff8ed265cfefedec1f570d5e6
sonhavietnamese <nguyensonha.hanz@gmail.com>
2025-12-29 10:54:06 +0700

    fix: neon adapter

--- apps/api/db/drizzle.ts
+++ apps/api/db/drizzle.ts
@@ -1,6 +1,8 @@
 import { config } from 'dotenv'
 import { drizzle } from 'drizzle-orm/neon-http'
+import { neon } from '@neondatabase/serverless'
 
 config({ path: '.env' }) // or .env.local
 
-export const db = drizzle(process.env.DATABASE_URL!)
+const sql = neon(process.env.DATABASE_URL!)
+export const db = drizzle(sql)
